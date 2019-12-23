546b82187efd9a150f65f0e4a3aaa72357540d8d
dj0nny <frank96990@gmail.com>
2019-12-23 13:57:07 +0100

    code refactoring #2

--- js/custom.js
+++ js/custom.js
@@ -42,11 +42,11 @@ const isDuplicate = (teamName) => {
 const autoCompletejs = new autoComplete({
   data: {
     src: async function () {
-      const source = await fetch("../json/pokèmon.json");
+      const source = await fetch('../json/pokèmon.json');
       const data = await source.json();
       return data;
     },
-    key: ["name"],
+    key: ['name'],
   },
   sort: function (a, b) {
     if (a.match < b.match) {
@@ -57,39 +57,39 @@ const autoCompletejs = new autoComplete({
     }
     return 0;
   },
-  selector: "#autoComplete",
+  selector: '#autoComplete',
   debounce: 0,
-  searchEngine: "strict",
+  searchEngine: 'strict',
   highlight: true,
   maxResults: 5,
   resultsList: {
     render: true,
     container: function (source) {
-      source.setAttribute("id", "autoComplete_list");
+      source.setAttribute('id', 'autoComplete_list');
     },
-    element: "ul",
-    destination: document.querySelector("#autoComplete"),
-    position: "afterend",
+    element: 'ul',
+    destination: document.querySelector('#autoComplete'),
+    position: 'afterend',
   },
   resultItem: {
     content: function (data, source) {
       source.innerHTML = data.match;
     },
-    element: "li",
+    element: 'li',
   },
   noResults: function () {
-    const result = document.createElement("li");
-    result.setAttribute("class", "no_result");
-    result.setAttribute("tabindex", "1");
-    result.innerHTML = "No Results";
-    document.querySelector("#autoComplete_list").appendChild(result);
+    const result = document.createElement('li');
+    result.setAttribute('class', 'no_result');
+    result.setAttribute('tabindex', '1');
+    result.innerHTML = 'No Results';
+    document.querySelector('#autoComplete_list').appendChild(result);
   },
   onSelection: function (feedback) {
-    document.querySelector("#autoComplete").blur();
+    document.querySelector('#autoComplete').blur();
     const selection = feedback.selection.value.name;
-    document.querySelector("#autoComplete").innerHTML = selection;
-    document.querySelector("#autoComplete").setAttribute("placeholder", selection);
-    document.querySelector("#autoComplete").value = "";
+    document.querySelector('#autoComplete').innerHTML = selection;
+    document.querySelector('#autoComplete').setAttribute('placeholder', selection);
+    document.querySelector('#autoComplete').value = '';
   },
 });
 
@@ -245,7 +245,7 @@ document.getElementById('add-team').addEventListener('submit', (event) => {
   event.preventDefault();
   const name = document.getElementById('team-name').value;
   if (name === '' ) {
-    alert("Team's name cannot be empty");
+    alert('The team name cannot be empty');
   } else {
     if (isDuplicate(name)) {
       alert(`There is already a team named ${name}`);
